fe0b855a6e79c559799bd9b5582fb3693c3f5937
Caio Braga <caiotabraga@hotmail.com>
2024-02-01 08:33:17 -0300

    Pegando dados da planilha e testando operações

--- CalculateApproval/Program.cs
+++ CalculateApproval/Program.cs
@@ -6,13 +6,47 @@ using Google.Apis.Sheets.v4.Data;
 using System;
 using System.IO;
 
-string googleclientId = "1125466948302 34529805";
-string googleclientSecret = "b8aa1fa7e1259f1ee0d4d19f80e716f0df54dfae\t";
-string[] scopes = new[] { Google.Apis.Sheets.v4.SheetsService.Scope.Spreadsheets };
-UserCredential credential = GoogleAuthentication.Login(googleclientId, googleclientSecret, scopes);
+string credPath = "C:\\Users\\RID - 4\\Desktop\\GoogleSheet\\GoogleSheetAPI\\CalculateApproval\\Credentials\\loyal-manifest-412818-b8aa1fa7e125.json"; 
 
-static string ApplicationName = "CalculateApproval";
+string[] scopes = { SheetsService.Scope.SpreadsheetsReadonly };
 
-private readonly SheetsService _sheetsService;
+GoogleCredential credential;
+using (var stream = new FileStream(credPath, FileMode.Open, FileAccess.Read))
+{
+    credential = GoogleCredential.FromStream(stream)
+        .CreateScoped(scopes);
+}
 
+var service = new SheetsService(new BaseClientService.Initializer()
+{
+    HttpClientInitializer = credential,
+    ApplicationName = "CalculateApproval",
+});
+
+string spreadsheetId = "1-iz4w7WxPP5Dj6I-UjL5t1Y9WDrXqgg_g7B2csJVRbo"; 
+
+string range = "engenharia_de_software!D4:E4"; 
+
+SpreadsheetsResource.ValuesResource.GetRequest request =
+        service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+ValueRange response = request.Execute();
+IList<IList<object>> values = response.Values;
+
+if (values != null && values.Count > 0)
+{
+    Console.WriteLine("Valores lidos da planilha:");
+    foreach (var row in values)
+    {
+        foreach (var col in row)
+        {
+            Console.Write("{0} ", col);
+        }
+        Console.WriteLine();
+    }
+}
+else
+{
+    Console.WriteLine("Nenhum dado encontrado.");
+}
 
