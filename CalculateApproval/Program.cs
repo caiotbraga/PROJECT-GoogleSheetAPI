d1c48ace62333327b25bccefca823554f8535dc9
Caio Braga <caiotabraga@hotmail.com>
2024-02-04 00:54:40 -0300

    Implementando regra de validacao de status de aprovacao

--- CalculateApproval/Program.cs
+++ CalculateApproval/Program.cs
@@ -6,7 +6,7 @@ using Google.Apis.Sheets.v4.Data;
 using System;
 using System.IO;
 
-string credPath = "C:\\Users\\RID - 4\\Source\\Repos\\GoogleSheetAPI\\CalculateApproval\\Credentials\\loyal-manifest-412818-b8aa1fa7e125.json";
+string credPath = "/Users/caiotbraga/Desktop/GOOGLE/GoogleSheetAPI/CalculateApproval/Credentials/loyal-manifest-412818-b8aa1fa7e125.json";
 
 string[] scopes = { SheetsService.Scope.Spreadsheets};
 
@@ -24,22 +24,61 @@ var service = new SheetsService(new BaseClientService.Initializer()
 });
 
 string spreadsheetId = "1-iz4w7WxPP5Dj6I-UjL5t1Y9WDrXqgg_g7B2csJVRbo";
+int rowIndex = 3;
 
-string range = "engenharia_de_software!D4:F4";
+string rangeOfMissedClasses= "engenharia_de_software!C:C";
+SpreadsheetsResource.ValuesResource.GetRequest requestOfMissedClasses = service.Spreadsheets.Values.Get(spreadsheetId, rangeOfMissedClasses);
+ValueRange responseOfMissedClasses = requestOfMissedClasses.Execute();
+IList<IList<object>> valuesOfMissedClasses = responseOfMissedClasses.Values;
 
-SpreadsheetsResource.ValuesResource.GetRequest request =
-        service.Spreadsheets.Values.Get(spreadsheetId, range);
+if(valuesOfMissedClasses != null && valuesOfMissedClasses.Count > 0)
+{
+    foreach (var row in valuesOfMissedClasses)
+    {
+        int numberOfClasses = 0;
+        int numberOfAbsences = 0;
+        string status;
 
+        foreach (var col in row)
+        {
+            if (col.ToString().Contains("Total de aulas no semestre:"))
+            {
+                string[] parts = col.ToString().Split(':');
+                numberOfClasses = int.Parse(parts[1].Trim());
+            }
+            else if (int.TryParse(col.ToString(), out int missed))
+            {
+                numberOfAbsences = missed;
+            }
+        }
+        
+        int numberOfMissedClassesAllowed = (numberOfClasses * 25) / 100;
+
+        if (numberOfAbsences > numberOfMissedClassesAllowed)
+        {
+            status = "Reprovado por falta";
+            string approvalStatusRange = "engenharia_de_software!G" + (rowIndex + 1);
+            var updateValues = new List<IList<object>> { new List<object> { status } };
+            var updateRequest = service.Spreadsheets.Values.Update(new ValueRange { Values = updateValues }, spreadsheetId, approvalStatusRange);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            var updateResponse = updateRequest.Execute();
+        }
+    }
+}
+
+string range = "engenharia_de_software!D4:F";
+SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
 ValueRange response = request.Execute();
 IList<IList<object>> values = response.Values;
 
 if (values != null && values.Count > 0)
 {
-    int rowIndex = 3;
     foreach (var row in values)
     {
         double sum = 0;
         int count = 0;
+        double average = sum / count;
+        string status;
         foreach (var col in row)
         {
             if (double.TryParse(col.ToString(), out double grade))
@@ -48,28 +87,25 @@ if (values != null && values.Count > 0)
                 count++;
             }
         }
-
-        double average = sum / count;
-
-        string studentAverageRange = "engenharia_de_software!H" + (rowIndex + 1); 
-        var updateValues = new List<IList<object>> { new List<object> { average } };
-        var updateRequest = service.Spreadsheets.Values.Update(new ValueRange { Values = updateValues }, spreadsheetId, studentAverageRange);
-        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
-        var updateResponse = updateRequest.Execute();
-
-        string status = average >= 70.0 ? "Aprovado" : (average >= 50.0 ? "Exame Final" : "Reprovado por Nota");
-
-        string approvalStatusRange = "engenharia_de_software!G" + (rowIndex + 1); 
-        updateValues = new List<IList<object>> { new List<object> { status } };
-        updateRequest = service.Spreadsheets.Values.Update(new ValueRange { Values = updateValues }, spreadsheetId, approvalStatusRange);
-        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
-        updateResponse = updateRequest.Execute();
-
+        status = average >= 70.0 ? "Aprovado" : (average >= 50.0 ? "Exame Final" : "Reprovado por Nota");
+        string approvalStatusRange = "engenharia_de_software!G" + (rowIndex + 1);
+        SpreadsheetsResource.ValuesResource.GetRequest statusRequest =
+        service.Spreadsheets.Values.Get(spreadsheetId, approvalStatusRange);
+        ValueRange statusResponse = statusRequest.Execute();
+        IList<IList<object>> statusValues = statusResponse.Values;
+        if(statusValues == null)
+        {
+            var updateValues = new List<IList<object>> { new List<object> { status } };
+            var updateRequest = service.Spreadsheets.Values.Update(new ValueRange { Values = updateValues }, spreadsheetId, approvalStatusRange);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            var updateResponse = updateRequest.Execute();
+        }
         rowIndex++;
     }
 }
 else
 {
     Console.WriteLine("Nenhum dado encontrado.");
+    
 }
 
