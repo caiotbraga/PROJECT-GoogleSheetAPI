84e401ae122c5132a05918c4cad72651abda8c27
Caio Braga <caiotabraga@hotmail.com>
2024-02-04 02:11:21 -0300

    Aplicacao exibindo os status de cada aluno corretamente atraves de falta e nota

--- CalculateApproval/Program.cs
+++ CalculateApproval/Program.cs
@@ -24,44 +24,63 @@ var service = new SheetsService(new BaseClientService.Initializer()
 });
 
 string spreadsheetId = "1-iz4w7WxPP5Dj6I-UjL5t1Y9WDrXqgg_g7B2csJVRbo";
-int rowIndex = 3;
+int rowIndex;
 
-string rangeOfMissedClasses= "engenharia_de_software!C:C";
-SpreadsheetsResource.ValuesResource.GetRequest requestOfMissedClasses = service.Spreadsheets.Values.Get(spreadsheetId, rangeOfMissedClasses);
-ValueRange responseOfMissedClasses = requestOfMissedClasses.Execute();
-IList<IList<object>> valuesOfMissedClasses = responseOfMissedClasses.Values;
+string rangeOfNumberOfClasses = "engenharia_de_software!A2:H2";
+SpreadsheetsResource.ValuesResource.GetRequest requestOfNumberOfClasses = service.Spreadsheets.Values.Get(spreadsheetId, rangeOfNumberOfClasses);
+ValueRange responseOfNumberOfClasses = requestOfNumberOfClasses.Execute();
+IList<IList<object>> valuesOfNumberOfClasses = responseOfNumberOfClasses.Values;
 
-if(valuesOfMissedClasses != null && valuesOfMissedClasses.Count > 0)
+int numberOfClasses = 0;
+if(valuesOfNumberOfClasses != null && valuesOfNumberOfClasses.Count > 0)
 {
-    foreach (var row in valuesOfMissedClasses)
+    foreach(var row in valuesOfNumberOfClasses)
     {
-        int numberOfClasses = 0;
-        int numberOfAbsences = 0;
-        string status;
-
-        foreach (var col in row)
+        foreach(var col in row)
         {
             if (col.ToString().Contains("Total de aulas no semestre:"))
             {
                 string[] parts = col.ToString().Split(':');
                 numberOfClasses = int.Parse(parts[1].Trim());
             }
-            else if (int.TryParse(col.ToString(), out int missed))
+        }
+    }
+}
+
+string rangeOfMissedClasses = "engenharia_de_software!C4:C";
+SpreadsheetsResource.ValuesResource.GetRequest requestOfMissedClasses = service.Spreadsheets.Values.Get(spreadsheetId, rangeOfMissedClasses);
+ValueRange responseOfMissedClasses = requestOfMissedClasses.Execute();
+IList<IList<object>> valuesOfMissedClasses = responseOfMissedClasses.Values;
+
+if (valuesOfMissedClasses != null && valuesOfMissedClasses.Count > 0)
+{
+    rowIndex = 4;
+    foreach (var row in valuesOfMissedClasses)
+    {
+        int numberOfAbsences = 0;
+        string status;
+        
+        foreach (var col in row)
+        {
+            if (int.TryParse(col.ToString(), out int missed))
             {
                 numberOfAbsences = missed;
             }
         }
-        
-        int numberOfMissedClassesAllowed = (numberOfClasses * 25) / 100;
-
+        int numberOfMissedClassesAllowed = (numberOfClasses * 25) / 100; 
         if (numberOfAbsences > numberOfMissedClassesAllowed)
         {
             status = "Reprovado por falta";
-            string approvalStatusRange = "engenharia_de_software!G" + (rowIndex + 1);
+            string approvalStatusRange = "engenharia_de_software!G" + (rowIndex);
             var updateValues = new List<IList<object>> { new List<object> { status } };
             var updateRequest = service.Spreadsheets.Values.Update(new ValueRange { Values = updateValues }, spreadsheetId, approvalStatusRange);
             updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
             var updateResponse = updateRequest.Execute();
+            rowIndex++;
+        }
+        else
+        {
+            rowIndex++;
         }
     }
 }
@@ -70,14 +89,14 @@ string range = "engenharia_de_software!D4:F";
 SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
 ValueRange response = request.Execute();
 IList<IList<object>> values = response.Values;
-
+rowIndex = 3;
+int student = 1;
 if (values != null && values.Count > 0)
 {
     foreach (var row in values)
     {
         double sum = 0;
         int count = 0;
-        double average = sum / count;
         string status;
         foreach (var col in row)
         {
@@ -87,6 +106,8 @@ if (values != null && values.Count > 0)
                 count++;
             }
         }
+        double average = sum / count;
+        Console.WriteLine("Média do aluno " + student++ + ": " + average);
         status = average >= 70.0 ? "Aprovado" : (average >= 50.0 ? "Exame Final" : "Reprovado por Nota");
         string approvalStatusRange = "engenharia_de_software!G" + (rowIndex + 1);
         SpreadsheetsResource.ValuesResource.GetRequest statusRequest =
@@ -99,6 +120,7 @@ if (values != null && values.Count > 0)
             var updateRequest = service.Spreadsheets.Values.Update(new ValueRange { Values = updateValues }, spreadsheetId, approvalStatusRange);
             updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
             var updateResponse = updateRequest.Execute();
+            
         }
         rowIndex++;
     }
